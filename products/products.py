e259f032bd337fab66a0d316cacd89a3e2fc3a30
Nick Liu <hybrida666@icloud.com>
2021-07-11 18:57:07 +0800

    add header

--- products/products.py
+++ products/products.py
@@ -4,8 +4,14 @@ while True:
 	if name == 'q':
 		break
 	dollar = input('How much was it:')
+	dollar = int(dollar.replace(',', ''))
 	products.append([name, dollar])
 print(products)
 
 for p in products:
 	print(p[0], 'is', p[1], 'dollars')
+
+with open ('products.csv', 'w') as f:
+	f.write('Products, Price' + '\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) + '\n')
