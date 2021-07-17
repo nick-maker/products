7058f0dcf30f4a9e1638447f43b911bb119610d6
Nick Liu <hybrida666@icloud.com>
2021-07-17 18:49:54 +0800

    add read file

--- products/products.py
+++ products/products.py
@@ -1,4 +1,15 @@
 products = []
+
+#讀取檔案
+with open('products.csv', 'r') as f:
+	for line in f:
+		if 'Products, Price' in line:
+			continue #繼續 ＃跳過這一回
+		name, dollar = line.strip().split(',')
+		products.append([name, dollar])
+print(products)
+
+#讓使用者輸入
 while True:
 	name = input('What did you buy:')
 	if name == 'q':
@@ -8,9 +19,11 @@ while True:
 	products.append([name, dollar])
 print(products)
 
+#印出所有購買紀錄
 for p in products:
 	print(p[0], 'is', p[1], 'dollars')
 
+#寫入檔案
 with open ('products.csv', 'w') as f:
 	f.write('Products, Price' + '\n')
 	for p in products:
