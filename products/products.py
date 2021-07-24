5a49abb9067ea3b9d47b3ba17dc7d3769f216f60
Nick Liu <hybrida666@icloud.com>
2021-07-25 02:09:11 +0800

    refactor

--- products/products.py
+++ products/products.py
@@ -1,36 +1,51 @@
 import os #operating system
 
 #讀取檔案
-products = []
-if os.path.isfile('products.csv'): #檢查檔案在不在
-	print('yap, file found!')
-	with open('products.csv', 'r') as f:
+def read_file(filename):
+	products = []
+	with open(filename, 'r') as f:
 		for line in f:
 			if 'Products, Price' in line:
 				continue #繼續 ＃跳過這一回
 			name, dollar = line.strip().split(',')
 			products.append([name, dollar])
-	print(products)
-else:
-	print('file not found...')
-
-
+	return products
+		
 #讓使用者輸入
-while True:
-	name = input('What did you buy:')
-	if name == 'q':
-		break
-	dollar = input('How much was it:')
-	dollar = int(dollar.replace(',', ''))
-	products.append([name, dollar])
-print(products)
+def user_input(products):
+	while True:
+		name = input('What did you buy:')
+		if name == 'q':
+			break
+		dollar = input('How much was it:')
+		dollar = int(dollar.replace(',', ''))
+		products.append([name, dollar])
+	print(products)
+	return products
 
 #印出所有購買紀錄
-for p in products:
-	print(p[0], 'is', p[1], 'dollars')
+def prints_products(products):
+	for p in products:
+		print(p[0], 'is', p[1], 'dollars')
 
 #寫入檔案
-with open ('products.csv', 'w') as f:
-	f.write('Products, Price' + '\n')
-	for p in products:
-		f.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):
+	with open (filename, 'w') as f:
+		f.write('Products, Price' + '\n')
+		for p in products:
+			f.write(p[0] + ',' + str(p[1]) + '\n')
+
+def main():
+	filename = 'products.csv'
+	if os.path.isfile(filename): #檢查檔案在不在
+		print('yap, file found!')
+		products = read_file(filename)
+	else:
+		print('file not found...')
+		
+	products = user_input(products)
+	prints_products(products)
+	write_file('products.csv', products)
+
+
+main()
